d98564346a2a7e17669ef91daf5eed756f6b05f4
taufiqSap <taufiqsatriaji@gmail.com>
2023-09-29 22:35:18 +0700

    Pertemuan 5

--- Pertemuan 5/Tugas1.java
+++ Pertemuan 5/Tugas1.java
@@ -1,21 +1,21 @@
 public class Tugas1 {
-    public double arr[];
-    int x[] = ;
-    public Tugas1(double arrNilai[]) {
-        arr = new double[arrNilai.length];
-        for (int i = 0; i < arr.length; i++) {
+    public int[] arr;
 
+    public Tugas1(int arrayNilai[]){
+        arr = new int[arrayNilai.length];
+        for (int i = 0; i < arrayNilai.length; i++) {
+            arr[i] = arrayNilai[i]; 
         }
     }
 
-    public int cari(double keyword) {
+    public int cari(int keyword){
         int index = -1;
         for (int i = 0; i < arr.length; i++) {
-            if (keyword == arr[i]) {
+            if(keyword == arr[i]){
                 index = i;
                 break;
-            } else {
-                if (keyword < arr[i]) {
+            }else{
+                if(keyword < arr[i]){
                     break;
                 }
             }
@@ -23,4 +23,18 @@ public class Tugas1 {
         return index;
     }
 
+    public void tampilkan(){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    public void temukan(int x, int index){
+        if(index != -1){
+            System.out.println("Data " + x + " pada index " + index);
+        }else{
+            System.out.println("Data " + x + " tidak ditemukan");
+        }
+    }
 }
--- Pertemuan 5/Tugas1Main.java
+++ Pertemuan 5/Tugas1Main.java
@@ -1,25 +1,16 @@
-import java.util.Scanner;
-public class Tugas1Main {
+public class Tugas1main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Input Element: ");
-        int elemen = sc.nextInt();
-
-        double [] data = {17,20,26,33,37,41,53,65,73,83};
-        for(int i = 0; i < data.length; i++){
-            System.out.print("input elemnt array of- "+ (i +1)+ " : ");
-            data[i] = sc.nextDouble();
-            Tugas1 tgs = new Tugas1(data);
-            System.out.println("fill array elemnt: ");
-            data[i] = sc.nextDouble();
-
-        
-
-        }
-        }
-        
-
+        int[] data = {17, 20, 26, 33, 37, 41, 53, 65, 73, 83};
+        Tugas1 tgs = new Tugas1(data);
+        System.out.println("Isi elemen array: ");
+        tgs.tampilkan();
+        int x = 41;
+        int index = tgs.cari(x);
+        tgs.temukan(x, index);
+        x = 50;
+        index = tgs.cari(x);
+        tgs.temukan(x, index);
     }
+}
     
 
